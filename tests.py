124cc308bb8f5e904a76aad2db3ef1f53eaa7c92
IF <illesf16@gmail.com>
2023-04-23 13:03:47 +0200

    Tests for symmetric voting games added.

--- tests.py
+++ tests.py
@@ -8,6 +8,18 @@ class ShapleyTest(unittest.TestCase):
     def test_small_bankruptcy_game(self):
         self.assertEqual(algorithm.Shapley(games.BankruptcyGame(9, [2,3,5,7])), [Fraction(13,12),Fraction(19,12),Fraction(31,12),Fraction(15,4)])
 
+    def test_small_symmetric_voting_game(self):
+        number_of_players, weight, quota = 5, 2, 8
+        g = games.VotingGame([weight]*number_of_players, quota)
+        sh = [Fraction(1,number_of_players)] * number_of_players 
+        self.assertEqual(algorithm.Shapley(g), sh)
+        
+    def test_large_symmetric_voting_game(self):
+        number_of_players, weight, quota = 20, 1, 8
+        g = games.VotingGame([weight]*number_of_players, quota)
+        sh = [Fraction(1,number_of_players)] * number_of_players 
+        self.assertEqual(algorithm.Shapley(g), sh)
+
 
 
 
