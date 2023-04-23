953174ac5405d15f08c00006837600edbdbd3736
IF <illesf16@gmail.com>
2023-04-23 02:29:06 +0200

    Final step: Shapley value calculation implemented.

--- algorithm.py
+++ algorithm.py
@@ -4,7 +4,7 @@ from sortedcontainers import SortedSet
 from collections import defaultdict
 
 
-def base_polynomials(arr: 'list[int]')-> 'tuple[dict[Polynomial], list[int]]':
+def base_polynomials(arr: 'list[int]') -> 'tuple[dict[Polynomial], list[int]]':
     """Takes the weights of the linear representation of a TU-game, (arr) and calculates the base polynomials. Returns a dictionary of the polynomials and a sorted list of keys for the nonzero polynomials."""
     polynomials = {0: Polynomial([Fraction(1)])}
     x = Polynomial([Fraction(0),Fraction(1)])
@@ -34,22 +34,26 @@ def decompose(base_poly, a:int) -> 'list[Polynomial]':
     return P
 
 
+def Shapley(game: 'Game') -> 'list[int]':
+    arr = game.a
+    B = base_polynomials(arr)
+    Sh = []
+    for a in arr:
+        pols = decompose(B, a)
+        p = sum([(game.f(j+a)-game.f(j))*pols[j] for j in pols])
+        coefs = p.coef
+        deg = len(coefs) - 1
+        sh = sum([Fraction(coefs[i],i+1) for i in range(1,deg+1)])
+        Sh.append(sh)
+    return Sh
 
 
 if __name__ == "__main__":
-    weights = [2,3,5,7]
-    b = base_polynomials(weights)
-    for k in b[0]:
-        print(k, "->", b[1][k])
-    print(".............")
-    for a in weights:
-        print("a=", a)
-        poly = decompose(b, a)
-        for k,p in poly.items():
-            print(k, "->", p)
-        print(".........")
-
-
-
+    import games
+    l, A = [2,3,5,7], 9
+    wvg = games.BankruptcyGame(A, l)
+    S = Shapley(wvg)
+    for s in S:
+        print(s)
 
 
